219ee151fc12918634b0f50d885c313a77b3e817
Low Saetern <saetern.low123@yahoo.com>
2022-10-11 16:54:46 -0700

    added zookeeper functionality

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,3 +1,5 @@
+const $zookeeperForm = document.querySelector('#zookeeper-form');
+
 const $animalForm = document.querySelector('#animal-form');
 
 const handleAnimalFormSubmit = event => {
@@ -18,6 +20,36 @@ const handleAnimalFormSubmit = event => {
   if (diet === undefined) {
     diet = '';
   }
+
+  const handleZookeeperFormSubmit = event => {
+    event.preventDefault();
+  
+    // get zookeeper data and organize it
+    const name = $zookeeperForm.querySelector('[name="zookeeper-name"]').value;
+    const age = parseInt($zookeeperForm.querySelector('[name="age"]').value);
+    const favoriteAnimal = $zookeeperForm.querySelector('[name="favorite-animal"]').value;
+  
+    const zookeeperObj = { name, age, favoriteAnimal };
+    console.log(zookeeperObj);
+    fetch('api/zookeepers', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(zookeeperObj)
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        alert('Error: ' + response.statusText);
+      })
+      .then(postResponse => {
+        console.log(postResponse);
+        alert('Thank you for adding a zookeeper!');
+      });
+  };
 
   const selectedTraits = $animalForm.querySelector('[name="personality"').selectedOptions;
   const personalityTraits = [];
@@ -45,6 +77,10 @@ const handleAnimalFormSubmit = event => {
       alert('Thank you for adding an animal!');
     });
 
+
 };
+
+$zookeeperForm.addEventListener('submit', handleZookeeperFormSubmit);
+
 
 $animalForm.addEventListener('submit', handleAnimalFormSubmit);
--- server.js
+++ server.js
@@ -3,12 +3,17 @@ const path = require('path');
 const express = require('express');
 const { animals } = require('./data/animals');
 
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
